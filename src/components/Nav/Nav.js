c8a913496f0dc9fb8588b1e86a0d7f9701973fa3
안도현 <andohyeon@andohyeon-ui-MacBookPro.local>
2021-06-24 17:31:34 +0900

    test2

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Nav extends Component {
   render() {
     return (
       <div>
-        Nav
+        <div>1</div>
+        <span>2</span>
       </div>
-    )
+    );
   }
 }
 
-export default Nav
+export default Nav;

6118873a67facae9bc478b38e4c2305cd80b5cde
Junghoon-P <qkr0454@naver.com>
2021-06-24 17:11:20 +0900

    Add: 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import LoginPark from "./pages/junghoon/Login/Login";
+import LoginChoi from "./pages/minjae/Login/Login";
+import LoginKim from "./pages/soojong/Login/Login";
+import MainPark from "./pages/junghoon/Main/Main";
+import MainChoi from "./pages/minjae/Main/Main";
+import MainKim from "./pages/soojong/Main/Main";
+
+import "./styles/reset.scss";
+import "./styles/common.scss";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/loginPark" component={LoginPark} />
+          <Route exact path="/loginChoi" component={LoginChoi} />
+          <Route exact path="/loginKim" component={LoginKim} />
+          <Route exact path="/mainPark" component={MainPark} />
+          <Route exact path="/mainChoi" component={MainChoi} />
+          <Route exact path="/mainKim" component={MainKim} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/minjae/Main/Aside/Aside.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Aside extends Component {
+  render() {
+    return (
+      <div>
+        aside
+      </div>
+    )
+  }
+}
+
+export default Aside
